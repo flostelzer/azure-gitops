a6e4deed7e189c221ba3637497e72e8ba51f369e
Florian Stelzer <flostelzer1@gmail.com>
2024-06-27 15:07:53 +0200

    fix: crossplane

--- infrastructure/aks_cluster.tf
+++ infrastructure/aks_cluster.tf
@@ -2,3 +2,9 @@ data "azurerm_kubernetes_cluster" "aks_automatic" {
   name                = var.aks_cluster.name
   resource_group_name = var.aks_cluster.rg_name
 }
+
+resource "azurerm_role_assignment" "creating_resources" {
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = "Owner"
+  principal_id         = data.azurerm_kubernetes_cluster.aks_automatic.kubelet_identity[0].object_id
+}
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -29,6 +29,8 @@ provider "azurerm" {
 
 data "azurerm_client_config" "client" {}
 
+data "azurerm_subscription" "primary" {}
+
 provider "external" {}
 
 provider "kubernetes" {
